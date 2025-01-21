5f7722efbbdc2607f9ab2e0b523460b8a576b04d
Josep Mulet Pol <jmuletpol@uoc.edu>
2025-01-21 10:37:13 +0100

    updated version number

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_widgethub';
-$plugin->release = '1.2.0';
+$plugin->release = '1.2.1';
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->version = 2024120800;
+$plugin->version = 2025012101;
