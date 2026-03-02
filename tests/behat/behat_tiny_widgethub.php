c3f909697afbf39537480ff49ed5246a149071cc
Josep Mulet Pol <jmuletpol@uoc.edu>
2026-03-02 11:03:29 +0100

    behat2

--- tests/behat/behat_tiny_widgethub.php
+++ tests/behat/behat_tiny_widgethub.php
@@ -1,20 +1,27 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
  * Behat steps definitions for your Moodle plugin.
  *
  * @package    tiny_widgethub
  * @category   test
- * @copyright  2026 Josep Mulet 
+ * @copyright  2026 Josep Mulet
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
-// NOTE: The class name MUST start with 'behat_' followed by the plugin name.
-// The file must be located in {plugin_root}/tests/behat/
-
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
-use Behat\Mink\Exception\ExpectationException;
 
 /**
  * Custom Behat steps for tiny_widgethub.
@@ -23,7 +30,7 @@ class behat_tiny_widgethub extends behat_base {
 
     /**
      * Changes the window size to a specific resolution.
-     * * Note: This is particularly useful for testing Moodle's 
+     * Note: This is particularly useful for testing Moodle's
      * responsive "Clean" or "Boost" themes in Docker.
      *
      * @Given /^I set the window size to "(?P<width>\d+)"x"(?P<height>\d+)"$/
