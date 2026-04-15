7e622156595a7bfadada7dcfe4f47ba7ec69e396
Josep Mulet Pol <jmuletpol@uoc.edu>
2026-04-15 11:33:13 +0200

    fix: lib.php

--- lib.php
+++ lib.php
@@ -21,9 +21,6 @@
  * @copyright   2026 Josep Mulet <pep.mulet@gmail.com>
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-// Keep MOODLE_INTERNAL check for backward compatibility with Moodle <4.3.
-// phpcs:ignore Moodle.Files.MoodleInternal.MoodleInternalNotNeeded
-defined('MOODLE_INTERNAL') || die();
 
 /**
  * Returns the user preferences for the Tiny WidgetHub plugin.
